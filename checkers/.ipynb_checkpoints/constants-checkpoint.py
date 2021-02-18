855b49c699072e00d9997360a4b976e4f89f3e8b
yasmine.nahal <yasmine.nahal@iktos.com>
2021-02-18 13:48:50 +0100

    Update files

---
+++ checkers/.ipynb_checkpoints/constants-checkpoint.py
@@ -0,0 +1,15 @@
+import pygame
+
+# checkers board parameters
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+WHITE = (255, 255, 255)
+YELLOW = (179, 135, 0)
+BROWN = (63, 42, 20)
+BLUE = (0, 0, 255)
+GREY = (128, 128, 128)
+
+CROWN = pygame.image.load('assets/crown.png')
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, WHITE
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, YELLOW
 from checkers.game import Game
-from minimax.minimax_algo import minimax
 
 FPS = 60 # in seconds
 
@@ -22,10 +21,6 @@ def main():
     
     while run:
         clock.tick(FPS) # to make sure the main event loop does not run too fast or too slow
-        
-        if game.turn == WHITE:
-            value, new_board = minimax(game.get_board(), 4, WHITE, game)
-            game.ai_move(new_board)
         
         if game.winner() != None:
             print(game.winner())
